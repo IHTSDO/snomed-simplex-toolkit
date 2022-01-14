4dc55f29ab7bcc8cdf3132f1e296053a8feb5584
Kai Kewley <kaikewley@gmail.com>
2022-01-14 16:06:36 +0000

    UI: add dev http proxy to API.

--- derivative-management-ui/vue.config.js
+++ derivative-management-ui/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    port: 8085,
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8081/',
+        changeOrigin: true
+      }
+    }
+  }
 }
