4d8eb1474de37a11ddbf7c4c33d4bc8a95076adf
Kai Kewley <kaikewley@gmail.com>
2022-08-15 17:19:32 +0100

    Add menu icons.

--- simple-extension-toolkit-ui/src/components/MainTabs.vue
+++ simple-extension-toolkit-ui/src/components/MainTabs.vue
@@ -1,38 +1,40 @@
 <template>
   <v-container>
+    <ConnectToTermServer @connected="connect" @updateAppConfig="updateAppConfig"/>
     <v-tabs>
-      <v-tab href="#setup">Setup</v-tab>
-      <v-tab-item value="setup">
-        <ConnectToTermServer @connected="connect" @updateAppConfig="updateAppConfig"/>
-      </v-tab-item>
-      <v-tab href="#simple" :disabled="!connected">
-          Simple Refsets
-        </v-tab>
-      <v-tab-item value="simple">
-        <RefsetsManager v-bind:connected="connected" :refsetEndpoint="'simple'"/>
-      </v-tab-item>
-      <v-tab href="#simple-map-to-snomed-with-correlation" :disabled="!connected">
-        Simple Map to SNOMED with Correlation
+      <v-tab href="#simple">
+        <v-icon>mdi-playlist-star</v-icon>
+        Simple Refsets
       </v-tab>
-      <v-tab-item value="simple-map-to-snomed-with-correlation">
-        <RefsetsManager v-bind:connected="connected" :refsetEndpoint="'simple-map-to-snomed-with-correlation'"/>
+      <v-tab-item value="simple" :disabled="!connected">
+        <RefsetsManager v-bind:connected="connected" 
+          :refsetEndpoint="'simple'"
+          :title="''"/>
       </v-tab-item>
-      <v-tab href="#translation" :disabled="!connected">
-        Translation
+      <v-tab href="#maps" :disabled="!connected">
+        <v-icon>mdi-transfer-right</v-icon>
+        Map Refsets
       </v-tab>
-      <v-tab-item value="translation">
+      <v-tab-item value="maps">
+        <RefsetsManager v-bind:connected="connected" 
+          :refsetEndpoint="'simple-map-to-snomed-with-correlation'"
+          :title="'Simple maps with correlation to SNOMED CT'"/>
+      </v-tab-item>
+      <v-tab href="#translations" :disabled="!connected">
+        <v-icon>mdi-earth</v-icon>
+        Translations
+      </v-tab>
+      <v-tab-item value="translations">
         <TranslationManager :connectionDetails="connectionDetails"/>
       </v-tab-item>
       <v-tab href="#release" :disabled="!connected">
+        <v-icon>mdi-rocket</v-icon>
         Release &amp; Export
       </v-tab>
       <v-tab-item value="release">
         <ExportManager/>
       </v-tab-item>
     </v-tabs>
-    <v-tabs-items>
-      
-    </v-tabs-items>
   </v-container>
 </template>
 
@@ -71,7 +73,7 @@
   }
 </script>
 <style scoped>
-  /* h1 {
-    margin-top: 100px;
-  } */
+  .v-tab .v-icon {
+    margin-right: 10px;
+  }
 </style>
