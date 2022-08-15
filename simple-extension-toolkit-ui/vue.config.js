1caf4368a708020735e7e44077a7d464d07f2d81
Kai Kewley <kaikewley@gmail.com>
2022-08-15 17:20:33 +0100

    Fix dev server on localhost https

--- simple-extension-toolkit-ui/vue.config.js
+++ simple-extension-toolkit-ui/vue.config.js
@@ -5,6 +5,8 @@ module.exports = {
   publicPath: '/simplex-toolkit/',
   devServer: {
     port: 8085,
+    public: 'https://localhost',
+    disableHostCheck : true,
     proxy: {
       '^/api': {
         target: 'http://localhost:8081/',
