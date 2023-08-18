e9ab931759588ea45ec57647eff18a71bd73a331
Kai Kewley <kaikewley@gmail.com>
2023-08-18 15:57:41 +0100

    SIMPLEX-6 Automatically create Ontology expression

--- src/main/java/com/snomed/simplextoolkit/service/CodeSystemService.java
+++ src/main/java/com/snomed/simplextoolkit/service/CodeSystemService.java
@@ -5,15 +5,25 @@ import com.snomed.simplextoolkit.client.SnowstormClientFactory;
 import com.snomed.simplextoolkit.client.domain.CodeSystem;
 import com.snomed.simplextoolkit.client.domain.Concept;
 import com.snomed.simplextoolkit.client.domain.Concepts;
+import com.snomed.simplextoolkit.client.domain.RefsetMember;
+import com.snomed.simplextoolkit.domain.Page;
+import com.snomed.simplextoolkit.exceptions.ClientException;
 import com.snomed.simplextoolkit.exceptions.ServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+
+import static java.lang.String.format;
 
 @Service
 public class CodeSystemService {
+
+	public static final String OWL_ONTOLOGY_REFSET = "762103008";
+	public static final String OWL_EXPRESSION = "owlExpression";
+	public static final String OWL_ONTOLOGY_HEADER = "734147008";
 
 	@Autowired
 	private SnowstormClientFactory snowstormClientFactory;
@@ -24,11 +34,12 @@ public class CodeSystemService {
 		// Create code system
 		CodeSystem newCodeSystem = snowstormClient.createCodeSystem(name, shortName, namespace);
 
+		String moduleId = existingModuleId;
 		if (createModule) {
 			// Create module
 			String tag = "core metadata concept";
 			Concept tempModuleConcept = snowstormClient.createSimpleMetadataConcept(Concepts.MODULE, moduleName, tag, newCodeSystem);
-			String moduleId = tempModuleConcept.getConceptId();
+			moduleId = tempModuleConcept.getConceptId();
 			// Delete concept
 			snowstormClient.deleteConcept(tempModuleConcept, newCodeSystem);
 
@@ -43,6 +54,7 @@ public class CodeSystemService {
 			setDefaultModule(existingModuleId, newCodeSystem, snowstormClient);
 		}
 
+		createModuleOntologyExpression(moduleId, newCodeSystem, snowstormClient);
 
 		// TODO Update code system with module as uriModuleId - Only SnowstormX so far.
 		return newCodeSystem;
@@ -52,6 +64,25 @@ public class CodeSystemService {
 		Map<String, String> newMetadata = new HashMap<>();
 		newMetadata.put("defaultModuleId", moduleId);
 		snowstormClient.addBranchMetadata(newCodeSystem.getBranchPath(), newMetadata);
+	}
+
+	private void createModuleOntologyExpression(String moduleId, CodeSystem codeSystem, SnowstormClient snowstormClient) throws ServiceException {
+		List<RefsetMember> ontologyMembers = snowstormClient.getRefsetMembers(OWL_ONTOLOGY_REFSET, codeSystem, false, 0, 100).getItems();
+		RefsetMember existingOntologyExpressionMember = null;
+		for (RefsetMember ontologyMember : ontologyMembers) {
+			if (ontologyMember.isActive() && ontologyMember.getAdditionalFields().get(OWL_EXPRESSION).startsWith("Ontology(<http://snomed.info/sct/")) {
+				existingOntologyExpressionMember = ontologyMember;
+			}
+		}
+		if (existingOntologyExpressionMember == null) {
+			throw new ServiceException(format("Ontology expression is not found for code system %s", codeSystem.getShortName()));
+		}
+		String moduleOntologyExpression = format("Ontology(<http://snomed.info/sct/%s>)", moduleId);
+		if (!existingOntologyExpressionMember.getAdditionalFields().get(OWL_EXPRESSION).equals(moduleOntologyExpression)) {
+			existingOntologyExpressionMember.setActive(false);
+			RefsetMember newOntologyExpressionMember = new RefsetMember(OWL_ONTOLOGY_REFSET, moduleId, OWL_ONTOLOGY_HEADER).setAdditionalField(OWL_EXPRESSION, moduleOntologyExpression);
+			snowstormClient.createUpdateRefsetMembers(List.of(existingOntologyExpressionMember, newOntologyExpressionMember), codeSystem);
+		}
 	}
 
 	public void deleteCodeSystem(String shortName) throws ServiceException {
