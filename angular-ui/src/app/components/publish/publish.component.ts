df2d22db0b4a1a10bfe5fb6a18cb43e2188e8f7b
Alejandro Lopez Osornio <alopez72@gmail.com>
2023-08-23 11:48:40 -0300

    Publish OK

--- angular-ui/src/app/components/publish/publish.component.ts
+++ angular-ui/src/app/components/publish/publish.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-publish',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./publish.component.scss']
 })
 export class PublishComponent {
+  @Input() edition: string;
 
+  exportDelta() {
+    window.open('api/' + this.edition + '/rf2-export/delta');
+  }
 }
