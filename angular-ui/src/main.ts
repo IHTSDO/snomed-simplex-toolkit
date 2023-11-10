a8f3dda55f716c9fd596f8148938ce9c673d2ed2
Kai Kewley <kaikewley@gmail.com>
2023-11-10 08:32:22 +0000

    Enable frontend hot-reload for dev.

--- angular-ui/src/main.ts
+++ angular-ui/src/main.ts
@@ -4,7 +4,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import 'zone.js';
 
-enableProdMode();
-
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
