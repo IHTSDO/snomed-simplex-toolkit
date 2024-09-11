9e609ea58131bca11580f17afbe3c42fad214e2b
Kai Kewley <kaikewley@gmail.com>
2024-09-11 14:00:53 +0100

    Catch resource not found exception. Log at debug.

--- api/src/main/java/org/snomed/simplex/rest/ControllerAdvice.java
+++ api/src/main/java/org/snomed/simplex/rest/ControllerAdvice.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -71,6 +72,12 @@ public class ControllerAdvice {
 	public void handleClientAbortException(Exception exception) {
 		logger.info("A client aborted an HTTP connection, probably a page refresh during loading.");
 		logger.debug("ClientAbortException.", exception);
+	}
+
+	@ExceptionHandler(NoResourceFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public void handleNotFound(Exception exception) {
+		logger.debug("NoResourceFoundException", exception);
 	}
 
 	@ExceptionHandler(HTTPClientException.class)
