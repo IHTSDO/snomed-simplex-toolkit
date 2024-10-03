bbbcdb80aa5a4010823f1a369e7813d9bd178c78
alopezo <alopez72@gmail.com>
2024-10-03 10:44:45 -0300

    Added pagination to activities

--- angular-ui/src/app/components/edition-activities/edition-activities.component.ts
+++ angular-ui/src/app/components/edition-activities/edition-activities.component.ts
@@ -1,19 +1,26 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
 import { SimplexService } from 'src/app/services/simplex/simplex.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-edition-activities',
   templateUrl: './edition-activities.component.html',
-  styleUrl: './edition-activities.component.css'
+  styleUrls: ['./edition-activities.component.css']
 })
 export class EditionActivitiesComponent implements OnInit, OnChanges {
 
   @Input() edition: any;
-  activities: any;
+  activities: any[] = [];
   loading = false;
   skeleton: any[] = Array(2).fill({});
 
   displayedColumns: string[] = ['startDate', 'componentType', 'activityType', 'elapsed', 'icon'];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  pageSize = 5;
+  pageIndex = 0;
+  totalItems = 0;
 
   constructor(private simplexService: SimplexService) { }
 
@@ -23,8 +30,8 @@ export class EditionActivitiesComponent implements OnInit, OnChanges {
 
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['edition'] && changes['edition'].currentValue) {
-      // Check if the edition has changed
-      if (changes['edition'].previousValue && changes['edition'].currentValue.shortName !== changes['edition'].previousValue.shortName) {
+      if (!changes['edition'].previousValue || changes['edition'].currentValue.shortName !== changes['edition'].previousValue.shortName) {
+        this.pageIndex = 0;
         this.loadActivities();
       }
     }
@@ -32,16 +39,24 @@ export class EditionActivitiesComponent implements OnInit, OnChanges {
 
   loadActivities() {
     this.loading = true;
-    this.simplexService.getActivities(this.edition.shortName, 0, 5).subscribe(
+    const offset = this.pageIndex * this.pageSize;
+    this.simplexService.getActivities(this.edition.shortName, offset, this.pageSize).subscribe(
       (data: any) => {
         this.loading = false;
-        this.activities = data;
+        this.activities = data.items;
+        this.totalItems = data.total;
       },
       (error) => {
         this.loading = false;
         console.log('Error loading activities', error);
       }
     );
+  }
+
+  onPageChange(event: PageEvent) {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.loadActivities();
   }
 
 }
