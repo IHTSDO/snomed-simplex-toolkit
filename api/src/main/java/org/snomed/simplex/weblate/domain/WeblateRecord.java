e37bdb1ca3aa10ccd5cde63b156a2d9c9402bf43
Kai Kewley <kaikewley@gmail.com>
2025-01-10 12:38:12 +0000

    API: Create Weblate management API stubs

---
+++ api/src/main/java/org/snomed/simplex/weblate/domain/WeblateRecord.java
@@ -0,0 +1,5 @@
+package org.snomed.simplex.weblate.domain;
+
+public record WeblateRecord(String source, String target, String context, String developer_comments) {
+
+}
---
+++ api/src/main/java/org/snomed/simplex/weblate/domain/WeblateSet.java
@@ -0,0 +1,7 @@
+package org.snomed.simplex.weblate.domain;
+
+public record WeblateSet(String name, String slug, String project) {
+
+
+
+}
