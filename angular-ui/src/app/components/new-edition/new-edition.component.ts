b3c07a2a70507d7b5abd95cd0d5ac53135bdd0f7
alopezo <alopez72@gmail.com>
2025-01-31 10:03:27 -0300

    SIMPLEX-137: adding validation to edition name

--- angular-ui/src/app/components/new-edition/new-edition.component.ts
+++ angular-ui/src/app/components/new-edition/new-edition.component.ts
@@ -1,5 +1,5 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { lastValueFrom } from 'rxjs';
 import { SimplexService } from 'src/app/services/simplex/simplex.service';
@@ -10,14 +10,13 @@ import { SimplexService } from 'src/app/services/simplex/simplex.service';
   styleUrls: ['./new-edition.component.scss']
 })
 export class NewEditionComponent {
-
   saving = false;
 
   @Output() closePanel = new EventEmitter<void>();
   @Output() editionSaved = new EventEmitter<void>();
 
   form: FormGroup = this.fb.group({
-    name: ['', Validators.required],
+    name: ['', [Validators.required, Validators.maxLength(24), this.forbiddenWordsValidator]],
     shortName: ['', Validators.required],
     moduleId: [''],
     moduleName: [''],
@@ -34,6 +33,15 @@ export class NewEditionComponent {
     private snackBar: MatSnackBar
   ) { }
 
+  forbiddenWordsValidator(control: AbstractControl): ValidationErrors | null {
+    const forbiddenWords = ['edition', 'extension'];
+    const value = control.value?.toLowerCase();
+    if (value && forbiddenWords.some(word => value.includes(word))) {
+      return { forbiddenWords: true };
+    }
+    return null;
+  }
+
   submit() {
     this.form.markAllAsTouched();
     if (this.form.valid) {
@@ -46,10 +54,10 @@ export class NewEditionComponent {
         namespace: this.form.value.namespace
       };
       this.saving = true;
-      // Set the form to disabled
-      this.form.disable();
+      this.form.disable(); // Disable form to prevent multiple submissions
+
       lastValueFrom(this.simplexService.createEdition(edition)).then(
-        (edition) => {
+        () => {
           this.saving = false;
           this.closePanelEvent();
           this.editionSaved.emit();
@@ -57,6 +65,7 @@ export class NewEditionComponent {
         (error) => {
           console.error(error);
           this.saving = false;
+          this.form.enable(); // Re-enable form on error
           this.snackBar.open('Failed to create edition', 'Dismiss', {
             duration: 5000
           });
@@ -68,5 +77,4 @@ export class NewEditionComponent {
   closePanelEvent() {
     this.closePanel.emit();
   }
-
 }
