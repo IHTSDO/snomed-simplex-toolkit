4458e2470d43afc69e3294ed1dc8a3fe715b8471
Kai Kewley <kaikewley@gmail.com>
2025-02-04 13:14:47 +0000

    SIMPLEX-136 Sonacube fixes.

--- api/src/main/java/org/snomed/simplex/rest/WeblateComponentController.java
+++ api/src/main/java/org/snomed/simplex/rest/WeblateComponentController.java
@@ -11,7 +11,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
 @Tag(name = "Weblate Translation Management")
