d8169bd5b71db5b0f343b90406564742e8304bd8
Kai Kewley <kaikewley@gmail.com>
2025-03-17 21:41:37 +0000

    SIMPLEX-140 Refactor RT1 translation reading.

--- api/src/main/java/org/snomed/simplex/service/RefsetToolTranslationZipReader.java
+++ api/src/main/java/org/snomed/simplex/service/RefsetToolTranslationZipReader.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -38,7 +37,7 @@ public class RefsetToolTranslationZipReader implements TranslationUploadProvider
 		Map<Long, List<Description>> conceptMap = new Long2ObjectLinkedOpenHashMap<>();
 		Map<Long, Description.Acceptability> descriptionAcceptabilityMap = new Long2ObjectLinkedOpenHashMap<>();
 
-		boolean acceptabilityFound = false;
+		boolean anyAcceptabilityFound = false;
 		try (ZipInputStream zipInputStream = new ZipInputStream(rf2ZipFileInputStream)) {
 			ZipEntry zipEntry;
 			// Loop through zip entries until we have read the description and lang refset files. We don't know what order they will be in within the zip.
@@ -49,108 +48,126 @@ public class RefsetToolTranslationZipReader implements TranslationUploadProvider
 					continue;
 				}
 				if (zipEntryName.contains("sct2_Description_Snapshot")) {
-					// Read description file
-					BufferedReader descriptionReader = new BufferedReader(new InputStreamReader(zipInputStream));
-					String header = descriptionReader.readLine();
-					if (!header.equals("id\teffectiveTime\tactive\tmoduleId\tconceptId\tlanguageCode\ttypeId\tterm\tcaseSignificanceId")) {
-						throw new ServiceException(format("Unrecognised header in Description file '%s' within zip file.", zipEntryName));
-					}
-					String line;
-					int lineNum = 1;
-					while ((line = descriptionReader.readLine()) != null) {
-						lineNum++;
-						String[] split = line.split("\t");
-						// id	effectiveTime	active	moduleId	conceptId	languageCode	typeId	term	caseSignificanceId
-						// 0	1				2		3			4			5				6		7		8
-						if (!split[4].matches("\\d+")) {
-							logger.info("Bad conceptId format on line {}.", lineNum);
-							continue;
-						}
-						long conceptId = parseLong(split[4]);
-						String languageCode = split[5];
-						// Strip any dialect, just keep language code
-						languageCode = languageCode.substring(0, 2);
-
-						Description.CaseSignificance caseSignificance =
-								ignoreCaseInImport ? null : Description.CaseSignificance.fromConceptId(split[8]);
-						Description description = new Description(Description.Type.fromConceptId(split[6]), languageCode, split[7],
-								caseSignificance);
-
-						description.setDescriptionId(split[0]);
-						if (description.getType() == Description.Type.SYNONYM) {
-							conceptMap.computeIfAbsent(conceptId, key -> new ArrayList<>()).add(description);
-						}
-					}
-				} else if (zipEntryName.contains("der2_cRefset_LanguageSnapshot")) {
-					// Read lang refset file
-					BufferedReader langRefsetReader = new BufferedReader(new InputStreamReader(zipInputStream));
-					String header = langRefsetReader.readLine();
-					if (!header.equals("id\teffectiveTime\tactive\tmoduleId\trefsetId\treferencedComponentId\tacceptabilityId")) {
-						throw new ServiceException(format("Unrecognised header in Language Refset file '%s' within zip file.", zipEntryName));
-					}
-					String line;
-					int lineNum = 1;
-					while ((line = langRefsetReader.readLine()) != null) {
-						lineNum++;
-						String[] split = line.split("\t");
-						// id	effectiveTime	active	moduleId	refsetId	referencedComponentId	acceptabilityId
-						// 0	1				2		3			4			5						6
-
-						// Only read active rows in the refset we are interested in
-						if ("1".equals(split[2])) {
-							if (!split[5].matches("\\d+")) {
-								logger.info("Bad referencedComponentId format on line {}.", lineNum);
-								continue;
-							}
-							acceptabilityFound = true;
-							long descriptionId = parseLong(split[5]);
-							descriptionAcceptabilityMap.put(descriptionId, Description.Acceptability.fromConceptId(split[6]));
-						}
-					}
-
+					readDescriptions(zipInputStream, zipEntryName, conceptMap);
+				} else if (zipEntryName.contains("der2_cRefset_LanguageSnapshot") &&
+						readAcceptability(zipInputStream, zipEntryName, descriptionAcceptabilityMap)) {
+						anyAcceptabilityFound = true;
 				}
 			}
 
-			if (acceptabilityFound) {
-				// Merge acceptability into descriptions. Drop descriptions and concepts that has no active acceptability in the lang refset we are focused on.
-				Set<Long> conceptsWithNoInterestingDescriptions = new LongOpenHashSet();
-				for (Map.Entry<Long, List<Description>> conceptAndDescriptions : conceptMap.entrySet()) {
-					for (Description description : conceptAndDescriptions.getValue()) {
-						Description.Acceptability acceptability = descriptionAcceptabilityMap.get(parseLong(description.getDescriptionId()));
-						if (acceptability != null) {
-							description.setAcceptabilityMap(Map.of(langRefset, acceptability));
-						}
-						// Forget the description id from the refset tool
-						description.setDescriptionId(null);
-					}
-					// Forget about descriptions with no active acceptability in this lang refset
-					conceptAndDescriptions.setValue(conceptAndDescriptions.getValue().stream()
-							.filter(description -> description.getAcceptabilityMap() != null).collect(Collectors.toList()));
-					if (conceptAndDescriptions.getValue().isEmpty()) {
-						conceptsWithNoInterestingDescriptions.add(conceptAndDescriptions.getKey());
-					}
-				}
-				for (Long conceptsWithNoInterestingDescription : conceptsWithNoInterestingDescriptions) {
-					conceptMap.remove(conceptsWithNoInterestingDescription);
-				}
+			if (anyAcceptabilityFound) {
+				processAcceptability(conceptMap, descriptionAcceptabilityMap);
 			} else {
-				// No acceptability set. Assume first term of each type within each concept is preferred. The rest are acceptable.
-				logger.info("No acceptability found in Refset & Translation tool upload, setting first term preferred, others acceptable.");
-				for (Map.Entry<Long, List<Description>> conceptAndDescriptions : conceptMap.entrySet()) {
-					Set<Description.Type> typesFound = new HashSet<>();
-					for (Description description : conceptAndDescriptions.getValue()) {
-						if (typesFound.add(description.getType())) {
-							description.addAcceptability(langRefset, Description.Acceptability.PREFERRED);
-						} else {
-							description.addAcceptability(langRefset, Description.Acceptability.ACCEPTABLE);
-						}
-					}
-				}
+				assumeAcceptability(conceptMap);
 			}
 
 			return conceptMap;
 		} catch (IOException e) {
 			throw new ServiceException("Failed to read zip file.", e);
+		}
+	}
+
+	private void readDescriptions(ZipInputStream zipInputStream, String zipEntryName, Map<Long, List<Description>> conceptMap) throws IOException, ServiceException {
+		// Read description file
+		BufferedReader descriptionReader = new BufferedReader(new InputStreamReader(zipInputStream));
+		String header = descriptionReader.readLine();
+		if (!header.equals("id\teffectiveTime\tactive\tmoduleId\tconceptId\tlanguageCode\ttypeId\tterm\tcaseSignificanceId")) {
+			throw new ServiceException(format("Unrecognised header in Description file '%s' within zip file.", zipEntryName));
+		}
+		String line;
+		int lineNum = 1;
+		while ((line = descriptionReader.readLine()) != null) {
+			lineNum++;
+			String[] split = line.split("\t");
+			// id	effectiveTime	active	moduleId	conceptId	languageCode	typeId	term	caseSignificanceId
+			// 0	1				2		3			4			5				6		7		8
+			if (!split[4].matches("\\d+")) {
+				logger.info("Bad conceptId format on line {}.", lineNum);
+				continue;
+			}
+			long conceptId = parseLong(split[4]);
+			String languageCode = split[5];
+			// Strip any dialect, just keep language code
+			languageCode = languageCode.substring(0, 2);
+
+			Description.CaseSignificance caseSignificance =
+					ignoreCaseInImport ? null : Description.CaseSignificance.fromConceptId(split[8]);
+			Description description = new Description(Description.Type.fromConceptId(split[6]), languageCode, split[7],
+					caseSignificance);
+
+			description.setDescriptionId(split[0]);
+			if (description.getType() == Description.Type.SYNONYM) {
+				conceptMap.computeIfAbsent(conceptId, key -> new ArrayList<>()).add(description);
+			}
+		}
+	}
+
+	private boolean readAcceptability(ZipInputStream zipInputStream, String zipEntryName, Map<Long, Description.Acceptability> descriptionAcceptabilityMap) throws IOException, ServiceException {
+		// Read lang refset file
+		BufferedReader langRefsetReader = new BufferedReader(new InputStreamReader(zipInputStream));
+		String header = langRefsetReader.readLine();
+		if (!header.equals("id\teffectiveTime\tactive\tmoduleId\trefsetId\treferencedComponentId\tacceptabilityId")) {
+			throw new ServiceException(format("Unrecognised header in Language Refset file '%s' within zip file.", zipEntryName));
+		}
+		boolean anyAcceptabilityFound = false;
+		String line;
+		int lineNum = 1;
+		while ((line = langRefsetReader.readLine()) != null) {
+			lineNum++;
+			String[] split = line.split("\t");
+			// id	effectiveTime	active	moduleId	refsetId	referencedComponentId	acceptabilityId
+			// 0	1				2		3			4			5						6
+
+			// Only read active rows in the refset we are interested in
+			if ("1".equals(split[2])) {
+				if (!split[5].matches("\\d+")) {
+					logger.info("Bad referencedComponentId format on line {}.", lineNum);
+					continue;
+				}
+				anyAcceptabilityFound = true;
+				long descriptionId = parseLong(split[5]);
+				descriptionAcceptabilityMap.put(descriptionId, Description.Acceptability.fromConceptId(split[6]));
+			}
+		}
+		return anyAcceptabilityFound;
+	}
+
+	private void processAcceptability(Map<Long, List<Description>> conceptMap, Map<Long, Description.Acceptability> descriptionAcceptabilityMap) {
+		// Merge acceptability into descriptions. Drop descriptions and concepts that has no active acceptability in the lang refset we are focused on.
+		Set<Long> conceptsWithNoInterestingDescriptions = new LongOpenHashSet();
+		for (Map.Entry<Long, List<Description>> conceptAndDescriptions : conceptMap.entrySet()) {
+			for (Description description : conceptAndDescriptions.getValue()) {
+				Description.Acceptability acceptability = descriptionAcceptabilityMap.get(parseLong(description.getDescriptionId()));
+				if (acceptability != null) {
+					description.setAcceptabilityMap(Map.of(langRefset, acceptability));
+				}
+				// Forget the description id from the refset tool
+				description.setDescriptionId(null);
+			}
+			// Forget about descriptions with no active acceptability in this lang refset
+			conceptAndDescriptions.setValue(conceptAndDescriptions.getValue().stream()
+					.filter(description -> description.getAcceptabilityMap() != null).toList());
+			if (conceptAndDescriptions.getValue().isEmpty()) {
+				conceptsWithNoInterestingDescriptions.add(conceptAndDescriptions.getKey());
+			}
+		}
+		for (Long conceptsWithNoInterestingDescription : conceptsWithNoInterestingDescriptions) {
+			conceptMap.remove(conceptsWithNoInterestingDescription);
+		}
+	}
+
+	private void assumeAcceptability(Map<Long, List<Description>> conceptMap) {
+		// No acceptability set. Assume first term of each type within each concept is preferred. The rest are acceptable.
+		logger.info("No acceptability found in Refset & Translation tool upload, setting first term preferred, others acceptable.");
+		for (Map.Entry<Long, List<Description>> conceptAndDescriptions : conceptMap.entrySet()) {
+			Set<Description.Type> typesFound = new HashSet<>();
+			for (Description description : conceptAndDescriptions.getValue()) {
+				if (typesFound.add(description.getType())) {
+					description.addAcceptability(langRefset, Description.Acceptability.PREFERRED);
+				} else {
+					description.addAcceptability(langRefset, Description.Acceptability.ACCEPTABLE);
+				}
+			}
 		}
 	}
 
