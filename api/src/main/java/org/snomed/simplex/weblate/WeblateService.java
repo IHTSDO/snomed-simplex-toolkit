e0ccd1b8c0909226ba1983596be80b07acc237af
Kai Kewley <kaikewley@gmail.com>
2025-07-01 14:10:19 +0100

    SIMPLEX-138 Remove unused imports.

--- api/src/main/java/org/snomed/simplex/weblate/WeblateService.java
+++ api/src/main/java/org/snomed/simplex/weblate/WeblateService.java
@@ -27,7 +27,6 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
