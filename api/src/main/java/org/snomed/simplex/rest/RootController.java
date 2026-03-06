502a759cbce790aa0a29d076746eb4a09f77a85f
Kai Kewley <kaikewley@gmail.com>
2026-03-06 15:08:31 +0000

    SIMPLEX-201 Fix swagger redirect.

--- api/src/main/java/org/snomed/simplex/rest/RootController.java
+++ api/src/main/java/org/snomed/simplex/rest/RootController.java
@@ -2,6 +2,7 @@ package org.snomed.simplex.rest;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +17,9 @@ public class RootController {
 
 	@GetMapping("/")
 	@Operation(summary = "Root controller", hidden = true)
-	public void root(HttpServletResponse response) throws IOException {
-		response.sendRedirect("swagger-ui.html");
+	public void root(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String contextPath = request.getServletContext().getContextPath();
+		response.sendRedirect("%s/swagger-ui.html".formatted(contextPath));
 	}
 
 }
