143acf46c2d82e45bdc1eed678b82c1fc02f78bf
Kai Kewley <kaikewley@gmail.com>
2026-04-13 18:43:17 +0200

    SIMPLEX-216 Add diagram (generated on demand).

--- angular-ui/src/app/app.module.ts
+++ angular-ui/src/app/app.module.ts
@@ -1,17 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { HeaderInterceptor } from './interceptors/header.interceptor';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms'; // Import the ReactiveFormsModule
+import {provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
-import { SnomedNavbarComponent } from './components/snomed-navbar/snomed-navbar.component';
-import { SnomedFooterComponent } from './components/snomed-footer/snomed-footer.component';
-import { AuthenticationService } from './services/authentication/authentication.service';
-import { AuthoringService } from './services/authoring/authoring.service';
-import { EnvServiceProvider } from './providers/env.service.provider';
+import {SnomedNavbarComponent} from './components/snomed-navbar/snomed-navbar.component';
+import {SnomedFooterComponent} from './components/snomed-footer/snomed-footer.component';
+import {AuthenticationService} from './services/authentication/authentication.service';
+import {AuthoringService} from './services/authoring/authoring.service';
+import {EnvServiceProvider} from './providers/env.service.provider';
 import {ToastrModule} from 'ngx-toastr';
 import {StatusPageService} from './services/statusPage/status-page.service';
 import {AlphabeticalPipe} from './pipes/alphabetical/alphabetical.pipe';
@@ -22,70 +20,71 @@ import {ModalService} from './services/modal/modal.service';
 import {ModalComponent} from './components/modal/modal.component';
 import {TextFilterPipe} from './pipes/text-filter/text-filter.pipe';
 import {ConceptService} from './services/concept/concept.service';
-import { MainViewComponent } from './components/main-view/main-view.component';
-import { CodesystemsComponent } from './components/codesystems/codesystems.component';
-import { BootConfigComponent } from './components/boot-config/boot-config.component';
-import { SyndicationComponent } from './components/syndication/syndication.component';
-import { RbacComponent } from './components/rbac/rbac.component';
-import { StringComponent } from './components/codesystems/types/string/string.component';
-import { NumberComponent } from './components/codesystems/types/number/number.component';
-import { BooleanComponent } from './components/codesystems/types/boolean/boolean.component';
-import { ArrayComponent } from './components/codesystems/types/array/array.component';
-import { ObjectComponent } from './components/codesystems/types/object/object.component';
+import {MainViewComponent} from './components/main-view/main-view.component';
+import {CodesystemsComponent} from './components/codesystems/codesystems.component';
+import {BootConfigComponent} from './components/boot-config/boot-config.component';
+import {SyndicationComponent} from './components/syndication/syndication.component';
+import {RbacComponent} from './components/rbac/rbac.component';
+import {StringComponent} from './components/codesystems/types/string/string.component';
+import {NumberComponent} from './components/codesystems/types/number/number.component';
+import {BooleanComponent} from './components/codesystems/types/boolean/boolean.component';
+import {ArrayComponent} from './components/codesystems/types/array/array.component';
+import {ObjectComponent} from './components/codesystems/types/object/object.component';
 import {MatTabsModule} from '@angular/material/tabs';
 import {MatIconModule} from '@angular/material/icon';
-import { SelectEditionComponent } from './components/select-edition/select-edition.component';
+import {SelectEditionComponent} from './components/select-edition/select-edition.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
 import {MatListModule} from '@angular/material/list';
-import { MatRippleModule } from '@angular/material/core';
-import { SubsetsComponent } from './components/subsets/subsets.component';
-import { MapsComponent } from './components/maps/maps.component';
-import { TranslationsComponent } from './components/translations/translations.component';
-import { ManageCodesystemComponent } from './components/manage-codesystem/manage-codesystem.component';
+import {MatRippleModule} from '@angular/material/core';
+import {SubsetsComponent} from './components/subsets/subsets.component';
+import {MapsComponent} from './components/maps/maps.component';
+import {TranslationsComponent} from './components/translations/translations.component';
+import {ManageCodesystemComponent} from './components/manage-codesystem/manage-codesystem.component';
 import {MatButtonModule} from '@angular/material/button';
-import { NewEditionComponent } from './components/new-edition/new-edition.component';
-import { ReactiveFormsModule } from '@angular/forms'; // Import the ReactiveFormsModule
+import {NewEditionComponent} from './components/new-edition/new-edition.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
-import { CamelToTitlePipe } from './pipes/camel-to-title/camel-to-title.pipe';
+import {CamelToTitlePipe} from './pipes/camel-to-title/camel-to-title.pipe';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
-import { ArtifactsComponent } from './components/artifacts/artifacts.component';
+import {ArtifactsComponent} from './components/artifacts/artifacts.component';
 import {MatSelectModule} from '@angular/material/select';
-import { JobsComponent } from './components/jobs/jobs.component';
-import { TimeAgoPipe } from './pipes/timeAgo/time-ago.pipe';
-import { ConceptsListComponent } from './components/artifacts/concepts-list/concepts-list.component';
-import { MatPaginatorModule } from '@angular/material/paginator';
+import {JobsComponent} from './components/jobs/jobs.component';
+import {TimeAgoPipe} from './pipes/timeAgo/time-ago.pipe';
+import {ConceptsListComponent} from './components/artifacts/concepts-list/concepts-list.component';
+import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatRadioModule} from '@angular/material/radio';
-import { CookieService } from 'ngx-cookie-service';
-import { LegalAgreementComponent } from './components/legal-agreement/legal-agreement.component';
+import {CookieService} from 'ngx-cookie-service';
+import {LegalAgreementComponent} from './components/legal-agreement/legal-agreement.component';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatTooltipModule} from '@angular/material/tooltip';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import { TimelineComponent } from './components/timeline/timeline.component';
-import { ValidationResultsComponent } from './components/validation-results/validation-results.component';
+import {TimelineComponent} from './components/timeline/timeline.component';
+import {ValidationResultsComponent} from './components/validation-results/validation-results.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import { ProductPackagingComponent } from './components/product-packaging/product-packaging.component';
-import { WelcomeComponent } from './components/welcome/welcome.component';
-import { UpgradeEditionComponent } from './components/upgrade-edition/upgrade-edition.component';
-import { EditionActivitiesComponent } from './components/edition-activities/edition-activities.component';
-import { ElapsedPipe } from './pipes/elapsed/elapsed.pipe';
-import { DownloadReleasesComponent } from './components/download-releases/download-releases.component';
-import { AdminSettingsComponent } from './components/admin-settings/admin-settings.component';
-import { CommonModule } from '@angular/common';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { TranslationDashboardComponent } from './components/translation-dashboard/translation-dashboard.component';
-import { TranslationUnitEditComponent } from './components/translation-unit-edit/translation-unit-edit.component';
-import { AutocompleteBindingComponent } from './components/binding-controls/autocomplete-binding/autocomplete-binding.component';
-import { EclSelectionComponent } from './components/ecl-selection/ecl-selection.component';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {ProductPackagingComponent} from './components/product-packaging/product-packaging.component';
+import {WelcomeComponent} from './components/welcome/welcome.component';
+import {UpgradeEditionComponent} from './components/upgrade-edition/upgrade-edition.component';
+import {EditionActivitiesComponent} from './components/edition-activities/edition-activities.component';
+import {ElapsedPipe} from './pipes/elapsed/elapsed.pipe';
+import {DownloadReleasesComponent} from './components/download-releases/download-releases.component';
+import {AdminSettingsComponent} from './components/admin-settings/admin-settings.component';
+import {CommonModule} from '@angular/common';
+import {MatDialogModule} from '@angular/material/dialog';
+import {TranslationDashboardComponent} from './components/translation-dashboard/translation-dashboard.component';
+import {TranslationUnitEditComponent} from './components/translation-unit-edit/translation-unit-edit.component';
+import {AutocompleteBindingComponent} from './components/binding-controls/autocomplete-binding/autocomplete-binding.component';
+import {EclSelectionComponent} from './components/ecl-selection/ecl-selection.component';
 import {DrawerComponent} from "./components/drawer/drawer.component";
 import {DrawerService} from "./services/drawer/drawer.service";
 import {ConfigService} from "./services/config/config.service";
+
 // SERVICE IMPORTS
 
 
@@ -163,7 +162,8 @@ import {ConfigService} from "./services/config/config.service";
         MatProgressBarModule,
         MatCardModule,
         MatCheckboxModule,
-        MatAutocompleteModule], providers: [
+        MatAutocompleteModule,
+        MatSlideToggleModule], providers: [
         AuthenticationService,
         AuthoringService,
         StatusPageService,
--- api/src/main/java/org/snomed/simplex/rest/DiagramController.java
+++ api/src/main/java/org/snomed/simplex/rest/DiagramController.java
@@ -5,11 +5,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.snomed.simplex.client.SnowstormClient;
 import org.snomed.simplex.client.SnowstormClientFactory;
 import org.snomed.simplex.client.domain.CodeSystem;
+import org.snomed.simplex.client.domain.Concept;
 import org.snomed.simplex.diagram.DiagramService;
 import org.snomed.simplex.exceptions.ServiceException;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +40,18 @@ public class DiagramController {
 		SnowstormClient snowstormClient = snowstormClientFactory.getClient();
 		CodeSystem theCodeSystem = snowstormClient.getCodeSystemOrThrow(SnowstormClient.ROOT_CODESYSTEM);
 		return diagramService.createAndStoreDiagram(conceptId, snowstormClient, theCodeSystem);
+	}
+
+	@PostMapping("codesystems/{codeSystem}/diagrams/preview")
+	@Operation(summary = "Generate a concept diagram PNG from browser-format concept JSON")
+	@PreAuthorize("hasPermission('AUTHOR', #codeSystem)")
+	public ResponseEntity<byte[]> previewDiagram(@PathVariable String codeSystem, @RequestBody Concept concept) throws ServiceException {
+		SnowstormClient snowstormClient = snowstormClientFactory.getClient();
+		snowstormClient.getCodeSystemOrThrow(codeSystem);
+		byte[] png = diagramService.previewDiagram(concept);
+		return ResponseEntity.ok()
+				.contentType(MediaType.IMAGE_PNG)
+				.body(png);
 	}
 
 }
